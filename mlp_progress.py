7a120d5ea01125bffd7117635c2a69b9fc8e8ecf
Maks <agarkov05@inbox.ru>
2025-03-25 19:24:39 +0400

    User-PC

--- mlp_progress.py
+++ mlp_progress.py
@@ -22,7 +22,7 @@ y = dataset[target].values
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-ff
+
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 epochs_options = [100, 200, 300]
